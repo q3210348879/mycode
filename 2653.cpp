1087262b998b7421d68427dfb01c6e423e684d88
qjh <3210348879@qq.com>
2024-11-22 23:32:26 +0800

    2653

---
+++ 2653.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        
+    }
+};
+int main()
+{
+    vector<int> nums = {1, -1, -3, -2, 3};
+    int k = 3, x = 2;
+    Solution solution;
+    return 0;
+}
